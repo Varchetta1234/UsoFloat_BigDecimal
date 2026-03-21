e9fbe8e06c9d4dcf1eb59f4e1af48a23f6831d38
Varchetta1234 <gabriel1667@hotmail.com>
2026-03-20 21:47:50 -0300

    UPDATE

---
+++ aplicacion1/src/aplicacion1/bienvenido.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package aplicacion1;
+
+/**
+ * 
+ */
+public class bienvenido {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
